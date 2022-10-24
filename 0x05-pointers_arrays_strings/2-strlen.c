514740973e7e9e381317185765fe7f776465dc17
iekeng <ekeng7.eb@gmail.com>
2022-10-24 09:55:26 -0700

    include function that determines string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
-  *strlen - returns string length
-  *@s - function parameter
-  *Rrturn: length of string
+  *_strlen - returns string length
+  *@s: function parameter
+  *Return: length of string
   */
 int _strlen(char *s)
 {
 	int num = 0;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		num++;
 		++*s;
 	}
-	return(num);
+	return (num);
 }
 
