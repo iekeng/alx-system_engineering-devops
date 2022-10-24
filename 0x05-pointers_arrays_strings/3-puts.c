05571212cdcbb34ede5d0fe85cf780531754b961
iekeng <ekeng7.eb@gmail.com>
2022-10-24 15:50:20 -0700

    print string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *_puts - prints string to stdout
+  *@str: parameter passed to function
+  *Return: nothing
+  */
+void _puts(char *str)
+{
+	int num;
+
+	for(num=0; *str != '\0'; ++str)
+	{
+		_putchar(*(str + num));
+		num++;
+	}
+	_putchar('\n');
+
+}
+
