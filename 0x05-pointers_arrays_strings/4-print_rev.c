71e74bbeace6cecf077e618decb6941998689f8b
iekeng <ekeng7.eb@gmail.com>
2022-10-24 16:42:04 -0700

    function that prints strings in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
   *print_rev - print a sentence in reverse
-  *@count: determines the indices of @s
   *@s: input parameter
   *Return: nothing
   */
@@ -9,9 +8,12 @@ void print_rev(char *s)
 {
 	int count;
 
-	for (index = 0; s[count] != '\0'; ++count);
+	for (count = 0; s[count] != '\0'; ++count)
+		;
+
 	for (--count; count >= 0; --count)
-		_putchar(s[count])
+		_putchar(s[count]);
+
 	_putchar('\n');
-}	
+}
 
