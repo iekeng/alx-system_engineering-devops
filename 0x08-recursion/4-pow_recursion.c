70d9eb1022a5f5b09ea72a90fd68b7f1430604cb
iekeng <ekeng7.eb@gmail.com>
2022-11-03 13:39:43 -0700

    get exponents

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
-  *_pow_recursion - find the power of @x
-  *@x: base
-  *@y: exponent
-  *Return: final value of x
+  *_pow_recursion -returns the power of a number (@x)
+  *@x: first input parameter
+  *@y: second input parameter
+  *Return: @res
   */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
-		return (-1);
-	else if (y == 0)
+	int res = x;
+
+	if (y == 0)
 		return (1);
-	else 
-		return(x * _pow_recursion(x, y-1))
+	else if (y < 0)
+		return (-1);
+	else
+		res *= res * _pow_recursion(x, y - 1);
+		return (res);
 }
 
