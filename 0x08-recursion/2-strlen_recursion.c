7fa496c02e91b50aa54291696bdab73348d867ff
iekeng <ekeng7.eb@gmail.com>
2022-11-03 08:03:58 -0700

    modify string length function

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,9 +6,13 @@
   */
 int _strlen_recursion(char *s)
 {
-	if (s != '\0')
-		return (1 + _strlen_recursion(s + 1));
-	else
-		return (0);
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+		return (len);
 }
 
