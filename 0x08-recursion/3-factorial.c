bc521b2becc588bb0b97ad542b99b18ee37d1290
iekeng <ekeng7.eb@gmail.com>
2022-11-03 08:59:08 -0700

    modify factorial function

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,12 +6,11 @@
   */
 int factorial(int n)
 {	
-	int res = n;
-
 	if (n < 1)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	return (n * factorial(n-1));
+	else
+		return (n * factorial(n-1));
 }
 
