8413db543589bc3ef53c93b0be6c3f588165403a
iekeng <ekeng7.eb@gmail.com>
2022-11-05 08:16:30 -0700

    modify

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *
+  *
+  *
+  */
+void puts_half(char *str)
+{
+
+}
+
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *
+  *
+  *
+  */
+void print_array(int *a, int n)
+{
+
+}
+
