70a8c2ed55e7852cfb0b61aba09315bb19a5bebc
iekeng <ekeng7.eb@gmail.com>
2022-11-06 02:36:22 -0800

    fix Werror message

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,10 @@
   *@argv: argument vector
   *Return: 0
   */
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s \n", argv[0]);
+
+	return (0);
 }
 
