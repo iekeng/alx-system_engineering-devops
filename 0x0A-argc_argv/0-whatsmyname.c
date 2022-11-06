2ed7b9399a3ce74ee6486ff9b350ae1ade087cf5
iekeng <ekeng7.eb@gmail.com>
2022-11-05 23:40:30 -0700

    make programe print its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
-#include "main.h"
+#include <stdio.h>
 /**
-  *
-  *
-  *
+  *main - prints the name of program
+  *@argc: argument count
+  *@argv: argument vector
+  *Return: 0
   */
+int main(int argc, char *argv[])
+{
+	printf("%s \n", argv[0]);
+}
 
