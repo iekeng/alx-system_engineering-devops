a10a3b2a54350432006c278a792e08c3c0c4720a
iekeng <ekeng7.eb@gmail.com>
2022-11-07 04:46:56 -0800

    modify 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,19 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
-  *main - entry point
-  *@argc: argument counter
-  *@argv: argument vector
-  *Return: 0 always
-  */
+ * main - a program that prints the number of arguments
+ *        passed into it
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(int argc, char __attribute__((unused)) *argv[])
 {
-	int i;
+	int i = 0, m;
 
-	for (i = 0; i < argc; i++)
-		continue;
-	printf("%d\n", i);
-
+	while (i < argc)
+	{
+		m = i;
+		i++;
+	}
+	printf("%d\n", m);
 	return (0);
 }
-
