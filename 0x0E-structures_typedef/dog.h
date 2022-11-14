395fccf269c60d647cd23938ada4f39695f3b9e9
iekeng <ekeng7.eb@gmail.com>
2022-11-14 03:57:02 -0800

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *dog - structure to store dog details
+  *
+  *@name: dog's name
+  *@age: dog's age
+  *@owner: owner's name
+  *
+  *Reuturn: nothing
+  **/
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
