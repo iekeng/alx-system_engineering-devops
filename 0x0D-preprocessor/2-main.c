35727fdc5e4940a553902b2d824b28c455eb163d
iekeng <ekeng7.eb@gmail.com>
2022-11-15 05:00:06 -0800

    print program name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  *main - prints program name
+  *
+  *@argc: argument counter
+  *@argv: argument vector
+  *
+  *Return: 0
+  **/
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[6]);
+
+	return(0);
+}
+
