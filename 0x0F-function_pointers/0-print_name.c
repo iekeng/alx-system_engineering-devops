a1148c2135110d50ce0f2692e417a66441f9d99c
iekeng <ekeng7.eb@gmail.com>
2022-11-16 17:02:19 -0800

    print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,8 +3,8 @@
 /**
   *print_name - prints a name
   *
-  *@name - name to be printed
-  *@f - pointer to a funtion
+  *@name: name to be printed
+  *@f: pointer to a funtion
   *
   *Return: nothing
   *
@@ -14,6 +14,6 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL)
 		return (NULL);
-	f (name);
+	f(name);
 }
 
