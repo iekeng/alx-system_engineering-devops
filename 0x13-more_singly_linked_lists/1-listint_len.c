cfa03006a36e0bd8376f44818f28be585867156b
iekeng <ekeng7.eb@gmail.com>
2022-11-29 07:08:49 -0800

    get number of elements in a node

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**listint_len - Returns the number of elements in listint_t
+  *
+  *@h: A pointer to the head 
+  *
+  *
+  *Return: The number of elements in listint_t
+  *******************************************************/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while(h->n)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
+
