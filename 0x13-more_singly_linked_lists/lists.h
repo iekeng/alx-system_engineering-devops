7266f040782b31c92369c0c517be6a01337b597b
iekeng <ekeng7.eb@gmail.com>
2022-11-29 09:09:24 -0800

    make it work

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,7 +15,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
+
 int _putchar(char c);
 size_t listint_len(const listint_t *h);
 size_t print_listint(const listint_t *h);
