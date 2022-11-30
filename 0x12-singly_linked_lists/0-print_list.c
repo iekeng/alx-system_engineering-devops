20e505af0dbba572dca9ca2d76a59344d80d9c90
iekeng <ekeng7.eb@gmail.com>
2022-11-30 03:34:28 -0800

    make it work

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,19 +17,17 @@ size_t print_list(const list_t *h)
 	if (h == NULL)
 		return (0);
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		count++;
 
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, (nil));
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] (%s)\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
 	}
-	/*print for last node*/
-	printf("[%d] (%s)\n", h->len, h->str);
 
 	return (count);
 }
