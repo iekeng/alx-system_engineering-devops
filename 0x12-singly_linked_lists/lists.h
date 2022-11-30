a608d586452b599e54e6a04ec45d483643f8667f
iekeng <ekeng7.eb@gmail.com>
2022-11-30 02:33:33 -0800

    meet with alx requirements

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,23 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/**
+  *struct list_s - node structure
+  *list_t - typedef of list_s
+  *
+  *@str: string pointer
+  *@len: unsigned int data
+  *@next: pointer to next node
+  *
+  */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t
+
 #include <stdlib.h>
 #include <stdio.h>
 size_t print_list(const list_t *h);
