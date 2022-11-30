f8a89407999b9b59ba97b559bafd76f77f0f71e4
iekeng <ekeng7.eb@gmail.com>
2022-11-30 04:35:05 -0800

    return number of elements in list_t

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+  *list_len - returns number of elements in list_t
+  *
+  *@h: pointer to head
+  *
+  *Return: number of elements in list_t
+  */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
