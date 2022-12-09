b9f5c711ab45160415908d3231d3d7e476ff6997
iekeng <ekeng7.eb@gmail.com>
2022-12-09 03:56:15 -0800

    copies file content into another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -74,14 +74,15 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usafe: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	if (argv[1] == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argcv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	cp(argv[1], argv[2]);
 	return (0);
 }
+
