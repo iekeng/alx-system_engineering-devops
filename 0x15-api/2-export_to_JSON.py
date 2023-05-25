7f9f188f6d7b9785ee0cf01abc75c801b1a56635
iekeng <ekeng7.eb@gmail.com>
2023-05-25 14:01:54 +0000

    exports to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""export data to JSON"""
+import requests
+import sys
+import json
+
+if __name__ == '__main__':
+    baseUrl = 'https://jsonplaceholder.typicode.com/users'
+    employeeId = sys.argv[1]
+    url = baseUrl + "/" + employeeId
+    data = []
+
+    response = requests.get(url)
+    userName = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    for task in tasks:
+        taskDic = {'task': task['title'], 'completed': task['completed'], 'username': userName }
+        data.append(taskDic)
+
+    jsonData = {employeeId : data}
+
+    with open('{}.json'.format(employeeId), 'w') as f:
+        json.dump(jsonData, f)
+            
