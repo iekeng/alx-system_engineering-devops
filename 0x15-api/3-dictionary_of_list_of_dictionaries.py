4489d8a82c10ee1f93b49d6afaf1d569c0362730
iekeng <ekeng7.eb@gmail.com>
2023-05-26 13:30:56 +0000

    export custom data in json

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""export data to JSON"""
+import json
+import requests
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    response = requests.get(url)
+    users = response.json()
+    
+    with open('todo_all_employees.json', 'w') as file:
+        objs = {}
+        for user in users:
+            todoUrl = '{}{}/todos'.format(url, user['id'])
+            todo = requests.get(todoUrl)
+            username = user['username']
+            props = []
+            userId = ''
+            tasks = todo.json()
+            for task in tasks:
+                obj = {'task': task['title'], 'completed': task['completed'], 'username': username}
+                userId = task['userId']
+                props.append(obj)
+                objs[userId] = props
+            json.dump(objs, file)
+
+            
+        
+
