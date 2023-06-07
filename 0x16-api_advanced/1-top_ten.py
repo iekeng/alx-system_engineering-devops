0e6d05b240d8bdc2e6fa77bcdfe816646ab72ff1
iekeng <ekeng7.eb@gmail.com>
2023-06-07 04:46:46 +0100

    top ten hot posts from a subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """ Query the Reddit APi.
     Retrieve titles of the first ten posts
 """
-import request
+import requests
 
-def number_of_subscribers(subreddit):
-    
+
+def top_ten(subreddit):
+    """ Print top ten hot posts from a subreddit"""
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    header = {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
+    try:
+        response = requests.get(url, headers=header)
+        data = response.json()
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
+    except BaseException:
+        print(None)
