349967932d01024dd26dab8fab3e27f6f69e4617
iekeng <ekeng7.eb@gmail.com>
2023-06-07 22:49:19 +0100

    retrieve all titles for hot topics in a subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" Retrieve title of all hot posts """
+import requests
+
+
+def recurse(subreddit, hot_list=[], fullname=None):
+    """ recursively executes `title` task """
+    header = {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+        subreddit, fullname)
+    response = requests.get(url, headers=header)
+    data = response.json()
+    fullname = data['data']['after']
+
+    try:
+        if fullname is not None:
+            recurse(subreddit, hot_list, fullname)
+        for titles in data['data']['children']:
+            title = titles['data']['title']
+            hot_list.append(title)
+        print(hot_list)
+    except Exception as e:
+        print(e)
