1eb5f20342ce2ea4cc72fdcd82e0168dd372300a
iekeng <ekeng7.eb@gmail.com>
2023-06-07 07:44:49 +0100

    retrieve number of subscribers of a subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ collect subreddit data """
-
-    url = 'https://www.reddit.com/r/python/{}.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
     header = {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
+    num_subs = 0
     try:
         response = requests.get(url, headers=header)
         data = response.json()
-        num_subs = data['child'][1]['data']['subreddit_subscribers']
-        return num_subs
+        num_subs = data['data']['children'][0]['data']['subreddit_subscribers']
     except BaseException:
-        return (0)
+        return num_subs
