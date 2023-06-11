bce1ab076f79f6b306d0646b3600c9f4f37c95ec
iekeng <ekeng7.eb@gmail.com>
2023-06-11 04:45:45 +0000

    puppet config

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,17 +1,19 @@
+# Fix apache server
+
 $root_path = '/var/www/html'
 $file_list = [
-    	{path => '${root_path}/index.html'},
-	{path => '${root_path}/index.cgi'},
-	{path => '${root_path}/index.pl'},
-	{path => '${root_path}/.maintenance'},
-	{path => '${root_path}/wp-content/languages'},
-	{path => '${root_path}/wp-includes/languages'},
-	{path => '${root_path}/wp-content/db.php'},
-	{path => '${root_path}/wp-content/object-cache.php'},
-	{path => '${root_path}/wp-includes/class-wp-locale.phpp'},]
-				    
-	$file_list.each |$file| {
-		file { $file['path']:
-		       ensure => present,
-		}
-	}
+      {path => '${root_path}/index.html'},
+  {path => '${root_path}/index.cgi'},
+  {path => '${root_path}/index.pl'},
+  {path => '${root_path}/.maintenance'},
+  {path => '${root_path}/wp-content/languages'},
+  {path => '${root_path}/wp-includes/languages'},
+  {path => '${root_path}/wp-content/db.php'},
+  {path => '${root_path}/wp-content/object-cache.php'},
+  {path => '${root_path}/wp-includes/class-wp-locale.phpp'},]
+
+  $file_list.each |$file| {
+    file { $file['path']:
+           ensure => present,
+    }
+  }
