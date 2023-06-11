4f8516f89d105a746aec637ea5872a6400545a34
iekeng <ekeng7.eb@gmail.com>
2023-06-11 19:00:42 +0000

    bug-fix,

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,8 @@
 # fix LAMP stack 
 
 exec { 'replace_line':
-  command => "/usr/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  provider => shell,
+  command  => "sed -i 's/phpp/php/g' var/www/html/wp-settings.php",
 }
 
 service { 'apache2':
