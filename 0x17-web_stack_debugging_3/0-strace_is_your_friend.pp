f12441f6172dc644bfebcba42a383a59de0593a6
iekeng <ekeng7.eb@gmail.com>
2023-06-11 14:22:01 +0000

    fixing apache server

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,39 @@
-# Configure server and make corrections to typo error
- $file_to_edit = '/var/www/html/wp-settings.php'
+# fix missing files in LAMP stack
 
- exec { 'replace_line':
-   command => "sed -i 's/phpp/php/g' ${file_to_edit}"
+$root_path = '/var/www/html'
+
+file {"${root_path}/index.html":
+  ensure => present,
+}
+
+file {"${root_path}/index.cgi":
+  ensure => present,
+}
+
+file {"${root_path}/index.pl":
+  ensure => present,
+}
+
+file {"${root_path}/.maintenance":
+  ensure => present,
+}
+
+file {"${root_path}/wp-content/languages":
+  ensure => present,
+}
+
+file {"${root_path}/wp-content/db.php":
+  ensure => present,
+}
+
+file {"${root_path}/wp-content/object-cache.php":
+  ensure => present,
+}
+
+file {"${root_path}/wp-includes/class-wp-locale.phpp":
+  ensure => present,
+}
+
+file {"${root_path}/wp-includes/languages":
+  ensure => present,
 }
