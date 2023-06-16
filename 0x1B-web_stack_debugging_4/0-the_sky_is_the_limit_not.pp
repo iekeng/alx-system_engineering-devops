0e8fa331f30d0e98c05d28d2a4c47f74c6412763
iekeng <ekeng7.eb@gmail.com>
2023-06-16 20:00:33 +0100

    fix manifest

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 
 exec {'nginx fix':
   provider => shell,
-  command  => "sed 's/15/4096/g' /etc/default/nginx",
+  command  => "sed -i 's/15/4096/g' /etc/default/nginx",
 }
 
 exec {'nginx restart':
