2d8cb06a1552579d4f30bc1a06b9430de5fb0918
iekeng <ekeng7.eb@gmail.com>
2023-06-16 19:44:26 +0100

    fix indentation errors

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,10 +2,10 @@
 
 exec {'limits.conf':
   provider => shell,
-  command => "sed -i 's/5/4096/g; s/4/4096/g' /etc/security/limits.conf"
+  command  => "sed -i 's/5/4096/g; s/4/4096/g' /etc/security/limits.conf"
 }
 
 exec {'apply_config':
   provider => shell,
-  command => 'sysctl -p'
+  command  => 'sysctl -p'
 }
